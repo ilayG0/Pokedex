2c25af24bce3d8fb1ac729281a4fca4e4dfa790c
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-08 14:53:30 +0200

    - search bar design and html [added host styling] - hesader bug fix [render failed]

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./component/header/header";
+import { SearchBar } from "./component/search-bar/search-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
