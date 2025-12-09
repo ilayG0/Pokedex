efea2dd5023dc15eaf31186445e810cd881c7c80
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-09 09:28:10 +0200

    - added a loading poke ball while loading data

--- src/app/pages/pokemon-info/pokemon-info.component.ts
+++ src/app/pages/pokemon-info/pokemon-info.component.ts
@@ -1,27 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonService } from '../../services/pokemon.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { LoadingPokeBall } from '../../shared/loading-poke-ball/loading-poke-ball.component';
+import { Pokemon } from '../../models/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, LoadingPokeBall],
   templateUrl: './pokemon-info.component.html',
 })
 export class PokemonInfoComponent {
   private route = inject(ActivatedRoute);
   private pokemonService = inject(PokemonService);
 
-  pokemon: any = null;
-  isLoading = true;
+  pokemon!: Pokemon;
+  isLoading = signal(true);
 
   constructor() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.pokemonService.getPokemon(id).subscribe(p => {
+    this.pokemonService.getPokemon(id).subscribe((p) => {
       this.pokemon = p;
-      this.isLoading = false;
+      this.isLoading.set(false);
     });
   }
 }
